f8b62655e8eb8f6826253b9a3c297d322d0e8fb3
Andre <andrefranceschini@gmail.combitcoind>
2025-01-25 03:24:21 +0100

    Update

--- app2.py
+++ app2.py
@@ -1,12 +1,5 @@
 # main.py
-import sys
-import os
-
-# Add the /src directory to the Python path
-sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
-
-# Import necessary modules
-from src.gradio_interface import demo
+from src.gradio_interface import demo  # Direct import
 
 if __name__ == "__main__":
     # Launch the Gradio app
